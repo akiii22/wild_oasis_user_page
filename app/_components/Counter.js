2a81f2d6db5357b55d7026567f99bb74d4081257
jeromee <bulosanjerome50@gmail.com>
2025-01-15 07:44:59 +0800

    Initial commit in userpage

---
+++ app/_components/Counter.js
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  console.log(users);
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count < 1 ? count : count - 1)}>
+        Decrement
+      </button>
+      <div>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.username}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Guests Area",
+};
+
+export default function Page() {
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcom Jerome!
+    </h2>
+  );
+}
